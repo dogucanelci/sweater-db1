94af5d95c722d785f4c83d73f876dc1b36a620c6
dogucanelci <elci15@itu.edu.tr>
2021-05-08 01:16:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/client.js
@@ -0,0 +1,76 @@
+
+console.log("Selamlar kedişler! :)");
+const form = document.querySelector("form");
+const load = document.querySelector(".loading");
+const mewselement = document.querySelector(".mews");
+const API_URL = "http://localhost:5000/mews";
+load.style.display = "none";
+listAllmews();
+
+form.addEventListener("submit",(event)=>{
+
+    event.preventDefault();
+    console.log("you summited your twit succesfully!");
+    const formdata = new FormData(form);
+    const name = formdata.get("name");
+    const content = formdata.get("content");
+    const mew = {
+        name,content
+    };
+    console.log(mew);
+    form.style.display = "none";
+    load.style.display = "";
+    
+    
+    fetch(API_URL,{
+        method:"POST",
+        body: JSON.stringify(mew),
+        headers: {
+            "content-type": "application/json"
+        }
+
+    }).then(response => response.json())
+    .then(
+        createdMew => {
+            form.reset();
+            setTimeout(()=>{
+                form.style.display = "",
+                3000
+            })
+            form.style.display = "";
+//            console.log(createdMew);
+            listAllmews();
+            load.style.display = "none";
+        }
+    );
+});
+
+function listAllmews(){
+    mewselement.innerHTML = "";
+    fetch(API_URL)
+    .then(response=>response.json())
+    .then(mews=>{
+        mews.reverse();
+//        console.log(mews);
+        mews.forEach(mew => {
+            const div =document.createElement("div");
+            const header = document.createElement("h3");
+            header.textContent = mew.name;
+            const content = document.createElement("p");
+            content.textContent = mew.content;
+            const date = document.createElement("small");
+            const blanck = document.createElement("br");
+            date.textContent = new Date(mew.created);
+            div.appendChild(date);
+            div.appendChild(header);
+            div.appendChild(content);
+            div.appendChild(blanck);
+            mewselement.appendChild(div);
+
+            
+        });
+    });
+}
+
+
+
